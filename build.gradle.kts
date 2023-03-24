bfd55330b07bc4cdca4540da217ae2b539c94d64
Antonio Castillo Perez <antonio.castillo@my-rio.de>
2023-03-24 10:21:35 +0100

    First commit - Configuration and dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.7.5"
+	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
+}
+
+group = "cloud.rio"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-graphql:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.0.4")
+	implementation("org.springframework.boot:spring-boot-starter-websocket:3.0.4")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.0.4")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
